c9aeea33ccda20697812c37a5fd94e13187ee3ed
Chrysologue <ChrysoBeatz@gmail.com>
2026-03-21 07:09:14 +0300

    Implementing hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
+
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div
-      className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"
-      style={{ fontFamily: "var(--font-roboto-slab)" }}
-    >
-      Home Page
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Hero />
     </div>
   );
 }

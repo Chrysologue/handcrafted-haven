588a72d34e030c19ea821fd321ef179f592ae07f
Chrysologue <ChrysoBeatz@gmail.com>
2026-03-21 15:23:56 +0300

    Solving merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import ProductsCarousel from './products/products';
-
-export default function Home() {
-  return (
-    <div>
-      <ProductsCarousel />
-
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Hero />
+      <ProductsCarousel />
     </div>
   );
 }

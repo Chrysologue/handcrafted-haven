99e632ba432367818a8d05c0765bbf1e8c0993f0
Natnael31 <end24002byui.edu>
2026-04-09 09:53:17 +0300

    src/

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,9 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import ProductsCarousel from "./products";
 
 export default function ProductsPage() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get('search') || '';
-
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#f5f9e9] to-white">
-      <ProductsCarousel featuredOnly={false} title="All Products" search={search} />
+      <ProductsCarousel featuredOnly={false} title="All Products" />
     </div>
   );
 }
